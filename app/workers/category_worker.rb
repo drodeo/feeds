07e01a202f7176ffe03c7371f8660c93cecd6b29
dmitryR <paukov2967@gmail.com>
2017-12-06 00:34:36 +0300

    enhancement

--- app/workers/category_worker.rb
+++ app/workers/category_worker.rb
@@ -4,12 +4,15 @@ class CategoryWorker
 
 
   def perform
-    @cats=Category.where(pages_count: 0).pluck(:id)
+    @cats=Category.where(pages_count: 0).limit(500).pluck(:id)
     @cats.each do |cat|
-      Category.find(cat).delete if  Category.reset_counters(cat, :pages).nil?
+      if  Category.reset_counters(cat, :pages).nil?
+        Category.find(cat).delete
+
+      end
     end
     cat1=Category.exists?('Без категории')
-    @nocat=Page.where(category_id: nil)
+    @nocat=Page.where(category_id: 0)
     @nocat.update_all category_id: cat1.id
     Category.reset_counters(cat1.id, :pages)
   end
