476edd06a2baac8d516f8f363f8706b09f5bc148
dmitryR <paukov2967@gmail.com>
2017-12-27 14:12:08 +0300

    fix bugs

--- app/workers/category_worker.rb
+++ app/workers/category_worker.rb
@@ -4,13 +4,10 @@ class CategoryWorker
 
 
   def perform
-    cats=Category.order(pages_count: :asc).limit(500).pluck(:id)
+    cats=Category.where("pages_count >'100'").order(pages_count: :asc).limit(500).pluck(:id)
     cats.each do |cat|
       tmp= Category.reset_counters(cat, :pages)
     end
-    cat1=Category.exists?('Без категории')
-    nocat=Page.where(category_id: 0)
-    nocat.update_all category_id: cat1.id
 
     feeds=Feed.limit(500).pluck(:id)
     feeds.each do |feed|
