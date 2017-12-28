803327dd2e2193930aa6293921ec4e4e3ae2ba27
dmitryR <paukov2967@gmail.com>
2017-12-28 21:44:20 +0300

    add category worker delete

---
+++ app/workers/category_delete_worker.rb
@@ -0,0 +1,16 @@
+class CategoryDeleteWorker
+  include Sidekiq::Worker
+  include Sidekiq::Status::Worker # Important!
+
+
+  def perform
+    cats=Category.where("pages_count <'100'").limit(100).pluck(:id)
+    cats.each do |cat|
+      tmp= Category.find(cat)
+      cat1=Category.where(name: 'Без категории')
+      @pages=Page.where(category_id: tmp.id)
+      @pages.update_all category_id: cat1.id
+      Category(cat.id).delete
+    end
+  end
+end
--- app/workers/category_worker.rb
+++ app/workers/category_worker.rb
@@ -4,7 +4,7 @@ class CategoryWorker
 
 
   def perform
-    cats=Category.where("pages_count >'100'").order(pages_count: :asc).limit(500).pluck(:id)
+    cats=Category.where("pages_count >'100'").order(pages_count: :asc).limit(100).pluck(:id)
     cats.each do |cat|
       tmp= Category.reset_counters(cat, :pages)
     end
