f1fbb77ff1ca1e208339d9d3db57abbc72f07d32
dmitryR <paukov2967@gmail.com>
2017-12-29 16:08:18 +0300

    return rails admin

--- app/workers/category_delete_worker.rb
+++ app/workers/category_delete_worker.rb
@@ -5,12 +5,14 @@ class CategoryDeleteWorker
 
   def perform
     cats=Category.where("pages_count <'100'").limit(100).pluck(:id)
+    cat1=Category.where(name: 'Без категории').pluck(:id)
     cats.each do |cat|
       tmp= Category.find(cat)
-      cat1=Category.where(name: 'Без категории')
       @pages=Page.where(category_id: tmp.id)
-      @pages.update_all(category_id: cat1.first.id)
+      @pages.update_all(category_id: cat1)
       tmp.delete
     end
+    @pages=Page.where(category_id: nil).update_all(category_id: cat1)
+    @pages=Page.where(category_id: 0).update_all(category_id: cat1)
   end
 end
--- app/workers/category_worker.rb
+++ app/workers/category_worker.rb
@@ -6,12 +6,12 @@ class CategoryWorker
   def perform
     cats=Category.where("pages_count >'100'").order(pages_count: :asc).limit(100).pluck(:id)
     cats.each do |cat|
-      tmp= Category.reset_counters(cat, :pages)
+      puts tmp= Category.reset_counters(cat, :pages)
     end
 
     feeds=Feed.limit(500).pluck(:id)
     feeds.each do |feed|
-    Feed.reset_counters(feed, :pages)
+      puts Feed.reset_counters(feed, :pages)
     end
   end
 end
