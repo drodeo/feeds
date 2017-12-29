0d5ca0d2c919746f08aed70b8ca335af464fbc33
dmitryR <paukov2967@gmail.com>
2017-12-29 21:40:54 +0300

    category worker refactor

--- app/workers/category_worker.rb
+++ app/workers/category_worker.rb
@@ -6,7 +6,7 @@ class CategoryWorker
   def perform
     cats=Category.where("pages_count >'100'").order(pages_count: :desc).limit(100).pluck(:id)
     cats.each do |cat|
-      tmp=Category.find(cat)
+      tmp=Category.where(id: cat)
       tmp.update_all(pages_count: Page.where(category_id: cat).count)
     end
   end
