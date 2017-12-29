81373b7cc35c3738018c3d11729e441ec0d413d6
dmitryR <paukov2967@gmail.com>
2017-12-29 21:36:14 +0300

    category worker refactor

--- app/workers/category_worker.rb
+++ app/workers/category_worker.rb
@@ -1,24 +1,13 @@
-require 'ruby-prof'
 class CategoryWorker
   include Sidekiq::Worker
   include Sidekiq::Status::Worker # Important!
 
 
   def perform
-    RubyProf.start
     cats=Category.where("pages_count >'100'").order(pages_count: :desc).limit(100).pluck(:id)
     cats.each do |cat|
-
-      Category.reset_counters(cat, :pages)
+      tmp=Category.find(cat)
+      tmp.update_all(pages_count: Page.where(category_id: cat).count)
     end
-
-    feeds=Feed.limit(500).pluck(:id)
-    feeds.each do |feed|
-      Feed.reset_counters(feed, :pages)
-    end
-    result = RubyProf.stop
-    # print a flat profile to text
-    printer = RubyProf::FlatPrinter.new(result)
-    printer.print(STDOUT)
   end
 end
