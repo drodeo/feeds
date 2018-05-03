ec1f1939b060ece5c882b540ca6a58fb5bc6498c
DmitryR <paukov2967@gmail.com>
2018-05-03 20:20:17 +0300

    fix worker

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -7,16 +7,17 @@ class InfosController < ApplicationController
 
   def info
 
-    @pages = Page.where.not(published: nil ).count
-    @tags = ActsAsTaggableOn::Tag.where.not(name: nil).count
-    @taggings = ActsAsTaggableOn::Tagging.where(taggable_type: "Page").count
-    @source = Feed.count
-    @info=Info.first || Info.new
-    @info.page_count=@pages
-    @info.tag_count=@tags
-    @info.tagging=@taggings
-    @info.size=@source
-    @info.save
+    #@pages = Page.where.not(published: nil ).count
+    #@tags = ActsAsTaggableOn::Tag.where.not(name: nil).count
+    #@taggings = ActsAsTaggableOn::Tagging.where(taggable_type: "Page").count
+    #@source = Feed.count
+    @info=Info.first
+    #@info.page_count=@pages
+    #@info.tag_count=@tags
+    #@info.tagging=@taggings
+    #@info.size=@source
+    #@info.save
+
 
   end
 
--- app/workers/category_worker.rb
+++ app/workers/category_worker.rb
@@ -4,9 +4,9 @@ class CategoryWorker
 
 
   def perform
-    cats=Category.where("pages_count > '?'", 100).order(pages_count: :desc).limit(100).pluck(:id)
+    cats=Category.where("pages_count > '?'", 100).order(pages_count: :desc).limit(100)
     cats.each do |cat|
-      Category.where(id: cat).update_all(pages_count: Page.where(category_id: cat).count)
+      Category.where(id: cat).update(pages_count: Page.where(category_id: cat).count, updated_at: Time.now) if cat.updated_at > Time.now+4
     end
   end
 end
